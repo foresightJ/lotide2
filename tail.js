975d4203227fd4d8b496853de47c18c30226e53d
John Foresight <foresight400@gmail.com>
2021-11-01 21:40:38 +0000

    still working on Lotide

--- tail.js
+++ tail.js
@@ -13,13 +13,8 @@ const assertEqual = function(actual, expected) {
 // assertEqual(1,2);
 
 const tail = function(arr) {
+  if(arr.length === 0) return ""
   return arr.slice(1);
 };
 
-const empty = [];
-const one = [1];
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(empty.length, 1);
-assertEqual(one.length, 0);
-assertEqual(words.length, 3);
+module.exports = tail;
---
+++ test/tailTest.js
@@ -0,0 +1,20 @@
+
+"use strict"
+
+const tail = require('../tail');
+const assertEqual = require('../assertEqual');
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+// const empty = tail([]);
+
+
+// assertEqual(result, ['Lighthouse', 'Labs']);
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
+// assertEqual(empty, "");
+
+
+
+
+
