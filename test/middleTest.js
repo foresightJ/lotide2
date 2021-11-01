6b6ae92e5283e5b1497dfe8738f54be75896cac9
John Foresight <foresight400@gmail.com>
2021-11-01 22:59:10 +0000

    rounding up Lotide

---
+++ test/middleTest.js
@@ -0,0 +1,24 @@
+
+"use strict"
+
+// const eqArrays = require('../eqArray')
+// const assertArraysEqual = require('../assertArraysEqual')
+const middle = require('../middle');
+const assertArraysEqual = require('../assertArraysEqual');
+
+
+
+// console.log(middle([1])) // => []
+// console.log(middle(([1, 2]))); // => []
+// console.log(middle([1, 2, 3])); // => [2]
+// console.log(middle([1, 2, 3, 4, 5])); // => [3]
+// console.log(middle([1, 2, 3, 4])) // => [2, 3]);
+// console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]);
+
+// assertArraysEqual(middle([1]), []);
+// assertArraysEqual(middle([1, 2]), []);
+
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2,]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
