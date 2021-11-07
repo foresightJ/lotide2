bf4b1db81df910218b6a01f928d74bec909de031
John Foresight <foresight400@gmail.com>
2021-11-07 14:06:53 +0000

    countletters working?

--- countLetters.js
+++ countLetters.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// const assertEqual = function(actual, expected) {
+// const assertEqual = function (actual, expected) {
 //   if (actual === expected) {
 //     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
 //   } else {
@@ -10,30 +10,22 @@
 
 // // TEST CODE
 // assertEqual("Bootcamp", "Bootcamp");
-// assertEqual(1,2);
-
-const countLetters = function(mySentence) {
-  // mySentence = mySentence.split().join(``);
-  let joinedSentence = mySentence.split(``).join(``);
-  let counter = {};
-  // The function should take in a sentence (as a string) 
-  for (const eachLetter of joinedSentence){
-    if (eachLetter){
-      if (!counter[eachLetter]){
-        counter[eachLetter] = 1;
-      } else counter[eachLetter] +=1;
-    }
-  }
-  console.log(`type of mySentence:`,typeof mySentence,` mySetence ;`,mySentence);
-  return counter;
-  //and then return a count of each of the letters in that sentence.
-}
+// assertEqual(1, 2);
 
 const mySentence = `How are you`;
-console.log(countLetters(mySentence));
 
-// const mySentence = String(["How", "are", "you"])
-// console.log(mySentence);
+const countLetters = function (mySentence) {
+  let joinedSentence = mySentence.split(``).join(``);
+  let counter = {};
 
-const joinedSentence = mySentence.split(' ').join('');
-console.log(joinedSentence)
+  for (const eachLetter of joinedSentence) {
+    if (eachLetter) {
+      if (!counter[eachLetter]) {
+        counter[eachLetter] = 1;
+      } else counter[eachLetter] += 1;
+    }
+  }
+  return counter;
+};
+
+console.log(countLetters(`How are you`));
